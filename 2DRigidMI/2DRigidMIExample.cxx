39083b31899c6b263113ff5d85ff48ad7d318547
Emily Hammond <emily-hammond@uiowa.edu>
2014-03-05 11:22:37 -0600

    COD: Finish registration code, display results and write out result

--- 2DRigidMI/2DRigidMIExample.cxx
+++ 2DRigidMI/2DRigidMIExample.cxx
@@ -33,18 +33,13 @@ int main( int argc, char * argv[] )
   typedef itk::ImageFileReader< FixedImageType >     FixedImageReaderType;
   typedef itk::ImageFileReader< MovingImageType >    MovingImageReaderType;
 
-  typedef itk::ImageFileWriter< FixedImageType >    WriterType;
-
-  // instantiate reader/writer objects
+  // instantiate reader objects
   FixedImageReaderType::Pointer fixedReader = FixedImageReaderType::New();
   MovingImageReaderType::Pointer movingReader = MovingImageReaderType::New();
 
-  WriterType::Pointer writer = WriterType::New();
-
-  // set parameters of reader/writer objects
+  // set parameters of reader objects
   fixedReader->SetFileName( argv[1] );
   movingReader->SetFileName( argv[2] );
-  writer->SetFileName( argv[3] );
   
   // set up registration component objects
   typedef itk::ImageRegistrationMethod< FixedImageType, MovingImageType > RegistrationType;
@@ -80,16 +75,75 @@ int main( int argc, char * argv[] )
   //   input into registrator
   registration->SetInitialTransformParameters( initialParameters );
 
+  // set up the optimizer and put in its parameters
+  optimizer->MinimizeOn();	// set optimizer up for minimization
+  optimizer->SetMaximumStepLength( 2.00 );
+  optimizer->GetMinimumStepLength( 0.01 );
+  optimizer->SetNumberOfIterations( 200 );
+  optimizer->SetRelaxationFactor( 0.8 );	// controls for the rate of step size reduction
   
-  
-  // set up pipeline
-
+  // set up pipeline to perform registration
   // set up registration inputs
   registration->SetFixedImage( fixedReader->GetOutput() );
   registration->SetMovingImage( movingReader->GetOutput() );
   registration->SetFixedImageRegion( fixedReader->GetOutput()->GetBufferedRegion() );
 
-  writer->SetInput( registration->GetOutput() );
+  // perform the registration
+  try
+  {
+      registration->Update();
+      std::cout << "OptimizerStepCondition: " << registration->GetOptimizer()->GetStopConditionDescription() << std::endl;
+  }
+  catch( itk::ExceptionObject & err )
+  {
+      std::cerr << "ExceptionObject caught!\n;
+      std::cerr << err << std::endl;
+      return EXIT_FAILURE;
+  }
+
+  // obtain and print out the final results
+  ParametersType finalParameters = registration->GetLastTransformParameters();
+  std::cout << "\nResults = \n";
+  std::cout << " Translation X = " << finalParameters[0] << std::endl;
+  std::cout << " Translation Y = " << finalParameters[1] << std::endl;
+  std::cout << " Iterations = " << optimizer->GetCurrentIterations() << std::endl;
+  std::cout << " Metric Value = " << optimizer->GetValue() << std::endl;
+  std::cout << " Stop Condition = " << optimizer->GetStopCondition() << std::endl;
+
+  // apply the resulting transform to the moving image using the ResampleImageFilter
+  typedef itk::ResampleImageFilter< MovingImageType, FixedImageType > ResampleFilterType;
+  // initialized a new transform data object
+  TransformType::Pointer finalTransform = TransformType::New();
+  //   set the parameters to those of the final result
+  finalTransform->SetParameters( finalParameters );
+  finalTransform->SetFixedParameters( transform->GetFixedParameters() );
+
+  // instantiate a resample object and a new original fixed image
+  ResampleImageFilter::Pointer resample = ResampleImageFilter::New();
+  FixedImageType::Pointer fixedImage = fixedReader->GetOutput();
+  // set up parameters to be those of the transform and identical to those of the fixed image
+  resample->SetTransform( finalTransform );
+  resample->SetSize( fixedImage->GetLargestPossibleRegion().GetSize() );
+  resample->SetOutputOrigin( fixedImage->GetOrigin() );
+  resample->SetOutputSpacing( fixedImage->GetSpacing() );
+  resample->SetDefaultPixelValue( 100 );
+  
+  // instantiate an output image to write to
+  typedef unsigned char  CharPixelType;
+  typedef itk::Image< CharPixelType, Dimension > OutputImageType;
+  typedef itk::ImageFileWriter< OutputImageType > WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[3] );
+
+  // set up to cast the image from a short to a char
+  typedef itk::CastImageFilter< ShortPixelType, CharImageType > CastFilterType;
+  CastFilterType::Pointer caster = CastFilterType::New();
+
+  // set up pipeline to casting and writing the image
+  resample->SetInput( movingReader->GetOutput() );
+  caster->SetInput( resample->GetOutput() );
+  writer->SetInput( caster->GetOutput() );
 
   try
   {
