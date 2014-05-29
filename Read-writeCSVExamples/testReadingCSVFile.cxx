ef6aabfc5f8b314cc1a71c723c3a21482fd94d20
Emily Hammond <emily-hammond@uiowa.edu>
2014-05-29 10:25:33 -0500

    INT: put into place testing code for reading/writing CSV files

---
+++ Read-writeCSVExamples/testReadingCSVFile.cxx
@@ -0,0 +1,16 @@
+/*
+ * Emily Hammond
+ * 5/29/14
+ *
+ * The purpose of this code is to demonstrate/debug the reading/writing of CSV files in C++.
+ *
+ */
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+int main( int argc, char * argv )
+{
+    return 0;
+}
