ec015afae094647e6da15ffede537949ddafc4ac
Emily Hammond <emily-hammond@uiowa.edu>
2014-05-30 15:43:12 -0500

    DOC: document reason for change to code

--- Read-writeCSVExamples/src/testReadingCSVFile.cxx
+++ Read-writeCSVExamples/src/testReadingCSVFile.cxx
@@ -4,6 +4,8 @@
  *
  * The purpose of this code is to demonstrate/debug the reading/writing of CSV files in C++.
  *
+ *NOTE: The csv file must be separated by spaces, not commas to allow for accurate comparison of image names********
+ *
  */
 
 #include <cstring>
