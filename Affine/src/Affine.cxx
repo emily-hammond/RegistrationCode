4dbf211d9ee6169636595c0234e074ec76999b53
Emily Hammond <emily-hammond@uiowa.edu>
2014-06-11 10:16:55 -0500

    COD: read in fixed/moving files

--- Affine/src/Affine.cxx
+++ Affine/src/Affine.cxx
@@ -14,5 +14,29 @@
 
 int main( int argc, char * argv[] )
 {
+    // obtain the necessary inputs from the command line
+    char * fisedFilename = argv[1];
+    char * movingFilename = argv[2];
+    char * outputFilename = argv[3];
+    char * fiducialFilename = argv[4];
+
+    // set up image parameters
+    const unsigned int  Dimension = 3;
+    typedef float       PixelType;
+    typedef itk::Image<PixelType, Dimension>    FixedImageType;
+    typedef itk::Image<FixelType, Dimension>    MovingImageType;
+
+    // read in images
+    typedef itk::ImageFileReader<FixedImageType>    FixedImageReaderType;
+    FixedImageReaderType::Pointer fixedReader = FixedImageReaderType::New();
+    fixedReader->SetFileName( fixedFilename );
+    fixedReader->Update();
+
+    typedef itk::ImageFileReader<MovingImageType>   MovingImageReaderType;
+    MovingImageReaderType::Pointer movingReader = MovingImageReaderType::New();
+    movingReader->SetFilenName( movingFilename );
+    movingReader->Update();
+
+
     return EXIT_SUCCESS;
 }
