8002530a3b3b9697fb359771cf1cda7acea1c934
Emily Hammond <emily-hammond@uiowa.edu>
2014-06-11 10:02:08 -0500

    INT: shell files for an affine registration

---
+++ Affine/src/Affine.cxx
@@ -0,0 +1,18 @@
+
+/*
+ *Author: Emily Hammond
+ *Date 6. 11. 2014
+ *
+ *The purpose of this registration is to simply perform an affine registration and determine how well it performed
+ *based on input fiducials.
+ *
+ */
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+int main( int argc, char * argv[] )
+{
+    return EXIT_SUCCESS;
+}
