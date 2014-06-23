adc0325caaabbb0f7d8230b0406b35b1af47dbb2
Emily Hammond <emily-hammond@uiowa.edu>
2014-06-23 12:15:47 -0500

    COD: reads in image and investigates index/point relationship

--- FiducialCode/src/investigateFiducials.cxx
+++ FiducialCode/src/investigateFiducials.cxx
@@ -12,6 +12,7 @@
 
 int main( int argc, char * argv[] )
 {
+    // obtain inputs
     char * filename = argv[1];
     int index0 = atoi( argv[2] );
     int index1 = atoi( argv[3] );
@@ -27,9 +28,11 @@ int main( int argc, char * argv[] )
     reader->SetFileName( filename );
     reader->Update();
 
-    ImageType::Pointer image = ImageType::New();
+    // create image
+    ImageType::Pointer image = reader->GetOutput();
+    std::cout << image << std::endl;
 
-    ImageType::PointType physicalPoint;
+    // obtain an index of the image
     ImageType::IndexType indexLocation;
 
     indexLocation[0] = index0;
@@ -38,6 +41,12 @@ int main( int argc, char * argv[] )
 
     std::cout << indexLocation << std::endl;
 
+    // obtain the pixel value at that image coordinate
+    ImageType::PixelType pixelValue = image->GetPixel( indexLocation );
+    std::cout << pixelValue << std::endl;
+
+    // see what the corresponding physical point is
+    ImageType::PointType physicalPoint;
     image->TransformIndexToPhysicalPoint( indexLocation, physicalPoint );
 
     std::cout << std::endl;
