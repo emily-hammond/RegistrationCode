1bed8ed4a8d96c8157ddc646ecc0b14d44388bdf
Emily Hammond <emily-hammond@uiowa.edu>
2014-06-23 10:26:17 -0500

    COD: read in image

--- FiducialCode/src/investigateFiducials.cxx
+++ FiducialCode/src/investigateFiducials.cxx
@@ -2,19 +2,27 @@
  * Emily Hammond
  * 5/29/14
  *
- * The purpose of this code is to demonstrate/debug the reading/writing of CSV files in C++.
- *
- *NOTE: The csv file must be separated by spaces, not commas to allow for accurate comparison of image names********
+ * The purpose of this code is to experiment with fiducials and how they coordinate between Slicer
+ * and ITK.
  *
  */
 
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <stdio.h>
+#include "itkImage.h"
+#include "itkImageFileReader.h"
 
 int main( int argc, char * argv[] )
 {
+    char * filename = argv[1];
+
+    // load in an image
+    const unsigned int  Dimension = 3;
+    typedef float       PixelType;
+    typedef itk::Image< PixelType, Dimension >  ImageType;
+    typedef itk::ImageFileReader< ImageType >   ReaderType;
+    ReaderType::Pointer reader = ReaderType::New();
+
+    reader->SetFileName( filename );
+    reader->Update();
 
     return 0;
 }
