7cdd0213c8d3373ec738e952be521261f2c1657a
ehammond <emily-hammond@uiowa.edu>
2014-08-15 15:12:34 -0500

    COD: write function to read images

--- src/main.cxx
+++ src/main.cxx
@@ -7,6 +7,36 @@
  *
  */
 
+// Write a function to read in images templated over dimension and pixel type
+template< const unsigned int Dimension, typename InputPixelType>
+int ReadInImage( const char * ImageFilename)
+{
+	// set up aliases
+	typedef InputPixelType						PixelType;
+	typedef itk::Image<PixelType, Dimension>	ImageType;
+	typedef itk::ImageFileReader<ImageType>		ReaderType;
+	
+	// create pointers to images
+	typename ReaderType::Pointer reader = ReaderType::New();
+	
+	// set parameters of reader
+	reader->SetFilename( ImageFilename );
+	
+	// update reader
+	try
+	{
+		reader->Update();
+	}
+	catch(itk::ExceptionObject & err)
+	{
+		std::cerr << "Exception Object Caught!" << std::endl;
+		std::cerr << err << std::endl;
+		std::cerr << std::endl;
+	}
+	
+	return EXIT_SUCCESS;
+}
+	
 
 int main( int argc, char * argv[] )
 {
