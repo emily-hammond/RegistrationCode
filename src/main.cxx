1308ef65e4432e8051786db6b3438d496f7f1b3a
ehammond <emily-hammond@uiowa.edu>
2014-08-15 15:54:18 -0500

    COD: write/test function to write out images

--- src/main.cxx
+++ src/main.cxx
@@ -7,20 +7,17 @@
  *
  */
 
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
 // Write a function to read in images templated over dimension and pixel type
-template< const unsigned int Dimension, typename InputPixelType>
-int ReadInImage( const char * ImageFilename)
+template<typename ImageType>
+typename ImageType::Pointer ReadInImage( const char * ImageFilename )
 {
-	// set up aliases
-	typedef InputPixelType						PixelType;
-	typedef itk::Image<PixelType, Dimension>	ImageType;
-	typedef itk::ImageFileReader<ImageType>		ReaderType;
-	
-	// create pointers to images
+	typedef itk::ImageFileReader<ImageType>		ReaderType;	
 	typename ReaderType::Pointer reader = ReaderType::New();
-	
-	// set parameters of reader
-	reader->SetFilename( ImageFilename );
+	reader->SetFileName( ImageFilename );
 	
 	// update reader
 	try
@@ -34,11 +31,51 @@ int ReadInImage( const char * ImageFilename)
 		std::cerr << std::endl;
 	}
 	
+	// return output
+	return reader->GetOutput();
+}
+
+// Write a function to write out images
+template<typename ImageType>
+int WriteOutImage( const char * ImageFilename,  )
+{
+	typedef itk::ImageFileWriter<ImageType>		WriterType;
+	typename WriterType::Pointer writer = WriterType::New();
+	writer->SetFileName( ImageFilename );
+
+	// update the writer
+	try
+	{
+		writer->Update();
+	}
+	catch(itk::ExceptionObject & err)
+	{
+		std::cerr << "Exception Object Caught!" << std::endl;
+		std::cerr << err << std::endl;
+		std::cerr << std::endl;
+	}
+	
+	// return output
 	return EXIT_SUCCESS;
 }
-	
 
 int main( int argc, char * argv[] )
 {
-    return 0;
+	// create specified image types to be used
+	typedef itk::Image<float, 3>			FloatImageType;
+	typedef itk::Image<unsigned char, 3>	CharImageType;
+
+	// take in arguments
+	char * InputFilename = argv[1];
+	char * OutputFilename = argv[2];
+
+	// read in the image
+	FloatImageType::Pointer inputImage = ReadInImage<FloatImageType>( InputFilename );
+	std::cout << "Image has been read in." << std::endl;
+
+	// write out image
+	WriteOutImage<CharImageType>( OutputFilename );
+	std::cout << "Image has been written." << std::endl;
+
+    return EXIT_SUCCESS;
 }
