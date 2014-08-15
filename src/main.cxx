d8f10126133dbe73bcd389e75b37561cce51298b
ehammond <emily-hammond@uiowa.edu>
2014-08-15 15:05:13 -0500

    INIT: shell code to write functions for reading images

---
+++ src/main.cxx
@@ -0,0 +1,14 @@
+/*
+ * Emily Hammond
+ * 8/15/2014
+ *
+ * Goals with this code
+ 1. Write functions that read in images, csv files and txt files
+ *
+ */
+
+
+int main( int argc, char * argv[] )
+{
+    return 0;
+}
