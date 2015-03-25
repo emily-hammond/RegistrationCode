e206fa5b3f316ef78b64650ed12f7709f08afd59
ehammond <emily-hammond@uiowa.edu>
2015-03-25 09:28:57 -0500

    INIT: create code to parse a text file with the desired inputs

---
+++ src/parseInputFile/parseInputFile.h
@@ -0,0 +1,39 @@
+/*
+Emily Hammond
+2015/03/25
+
+This is the declaration to parse an input file and gather all the inputs. Its state
+variables are the inputs taken from the file and those derived from the file, as well
+as the desired outputs. The methods include those required to read the text file and
+derived the additional information and print the inputs as well.
+
+*/
+
+#ifndef __ParseInputFile_h
+#define __ParseInputFile_h
+
+#include <itksys/SystemTools.hxx>
+#include <fstream>
+#include <cmath>
+
+class ParseInputFile: 
+{
+public:
+	
+protected:
+	// declare the constructor and define default parameters
+	ParseInputFile() 
+	{
+
+	}
+	
+	// destructor
+	virtual ~ParseInputFile() {}
+	
+private:
+	// declare variables
+
+	// private function that do most of the functionality
+};
+
+#endif
