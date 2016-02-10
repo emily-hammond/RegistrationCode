7bc7aaddd293c1ffbaea57ab540b93eb67d7fc73
ehammond <emily-hammond@uiowa.edu>
2016-02-10 15:08:02 -0600

    COD: transfer read and write functions to a header file

--- SourceDirectory/MultiLevelRegistration/main.cxx
+++ SourceDirectory/MultiLevelRegistration/main.cxx
@@ -3,10 +3,20 @@ INSERT COMMENTS HERE
 */
 
 // include files
+#include ReadWriteFunction.h
 
 
 int main( int argc, char * argv[] )
 {
+	//desired inputs
+	char * imageFilename = argv[1];
+	std::string outputDirectory = argv[2];
+
+	// test functionality of ReadWriteFunctions.h file
+	typedef itk::Image<unsigned short, 3>	ImageType;
+	ImageType::Pointer inputImage = ReadInImage< ImageType >( imageFilename );
+	std::string outputFilename  = outputDirectory + "\image.mhd";
+	WriteOutImage< ImageType, ImageType >( outputDirectory.c_str() );
 	
 	return EXIT_SUCCESS;
 }
