f40da7585ed030d6009b55eea6ecd854da2d1b0f
ehammond <emily-hammond@uiowa.edu>
2016-08-23 15:03:34 -0500

    COD: change preprocessing class to a header file of functions to be used in the main code

--- SourceDirectory/MultiLevelRegistrationSuper/itkPreprocessingFilter.hxx
+++ SourceDirectory/MultiLevelRegistrationSuper/itkPreprocessingFilter.hxx
@@ -1,79 +1,72 @@
-#ifndef __itkPreprocessingFilter_hxx
-#define __itkPreprocessingFilter_hxx
+#include "itkDiscreteGaussianImageFilter.h"
+#include "itkN4BiasFieldCorrectionImageFilter.h"
+#include "itkThresholdImageFilter.h"
 
-#include "itkPreprocessingFilter.h"
-
-namespace itk
+// templated function to threshold an image with an upper value
+template< typename ImageType > 
+typename ImageType::Pointer UpperThresholdImage( typename ImageType::Pointer image, int upperThreshold)
 {
-	// constructor
-	PreprocessingFilter::PreprocessingFilter()
-	{}
-	
-	// implement member functions
-	ImageType::Pointer PreprocessingFilter::UpperThresholdImage( ImageType::Pointer image, int upperThreshold)
+	typedef itk::ThresholdImageFilter< ImageType > 		ThresholdImageFilterType;
+	typename ThresholdImageFilterType::Pointer threshold = ThresholdImageFilterType::New();
+	threshold->SetInput( image );
+	threshold->ThresholdAbove( upperThreshold );
+	threshold->SetOutsideValue( upperThreshold );
+	try
 	{
-		typedef itk::ThresholdImageFilter< ImageType > 		ThresholdImageFilterType;
-		ThresholdImageFilterType::Pointer threshold = ThresholdImageFilterType::New();
-		threshold->SetInput( image );
-		threshold->ThresholdAbove( upperThreshold );
-		threshold->SetOutsideValue( upperThreshold );
-		try
-		{
-			threshold->Update();
-		}
-		catch (itk::ExceptionObject & err)
-		{
-			std::cerr << "Exception Object Caught!" << std::endl;
-			std::cerr << err << std::endl;
-			std::cerr << std::endl;
-		}
-		
-		return threshold->GetOutput();
+		threshold->Update();
+	}
+	catch (itk::ExceptionObject & err)
+	{
+		std::cerr << "Exception Object Caught!" << std::endl;
+		std::cerr << err << std::endl;
+		std::cerr << std::endl;
 	}
 	
-	ImageType::Pointer PreprocessingFilter::LowerThresholdImage( ImageType::Pointer image, int lowerThreshold)
-	{
-		typedef itk::ThresholdImageFilter< ImageType > 		ThresholdImageFilterType;
-		ThresholdImageFilterType::Pointer threshold = ThresholdImageFilterType::New();
-		threshold->SetInput( image );
-		threshold->ThresholdBelow( lowerThreshold );
-		threshold->SetOutsideValue( lowerThreshold );
-		try
-		{
-			threshold->Update();
-		}
-		catch (itk::ExceptionObject & err)
-		{
-			std::cerr << "Exception Object Caught!" << std::endl;
-			std::cerr << err << std::endl;
-			std::cerr << std::endl;
-		}
-		
-		return threshold->GetOutput();
-	}
-	
-	ImageType::Pointer PreProcessingFilter::SmoothImage( ImageType::Pointer image, int sigma )
-	{
-		typedef itk::DiscreteGaussianImageFilter< ImageType, ImageType >		SmoothingImageFilterType;
-		SmoothingImageFilterType::Pointer smooth = SmoothingImageFilterType::New();
-		smooth->SetInput( image );
-		smooth->SetVariance( sigma );
-		
-		try
-		{
-			smooth->Update();
-		}
-		catch (itk::ExceptionObject & err)
-		{
-			std::cerr << "Exception Object Caught!" << std::endl;
-			std::cerr << err << std::endl;
-			std::cerr << std::endl;
-		}
-		
-		return smooth->GetOutput();
-	}
-	
-		
-} // end namespace
+	return threshold->GetOutput();
+}
 
-#endif
+// write a function to threshold an image with a lower value
+template< typename ImageType >
+typename ImageType::Pointer PreprocessingFilter::LowerThresholdImage( typename ImageType::Pointer image, int lowerThreshold)
+{
+	typedef itk::ThresholdImageFilter< ImageType > 		ThresholdImageFilterType;
+	typename ThresholdImageFilterType::Pointer threshold = ThresholdImageFilterType::New();
+	threshold->SetInput( image );
+	threshold->ThresholdBelow( lowerThreshold );
+	threshold->SetOutsideValue( lowerThreshold );
+	try
+	{
+		threshold->Update();
+	}
+	catch (itk::ExceptionObject & err)
+	{
+		std::cerr << "Exception Object Caught!" << std::endl;
+		std::cerr << err << std::endl;
+		std::cerr << std::endl;
+	}
+	
+	return threshold->GetOutput();
+}
+
+// write a function to smooth an image with a gaussian image filter with specified sigma
+template< typename ImageType >
+typename ImageType::Pointer PreprocessingFilter::SmoothImage( typename ImageType::Pointer image, int sigma )
+{
+	typedef itk::DiscreteGaussianImageFilter< ImageType, ImageType >		SmoothingImageFilterType;
+	typename SmoothingImageFilterType::Pointer smooth = SmoothingImageFilterType::New();
+	smooth->SetInput( image );
+	smooth->SetVariance( sigma );
+	
+	try
+	{
+		smooth->Update();
+	}
+	catch (itk::ExceptionObject & err)
+	{
+		std::cerr << "Exception Object Caught!" << std::endl;
+		std::cerr << err << std::endl;
+		std::cerr << std::endl;
+	}
+	
+	return smooth->GetOutput();
+}
