bba4d5dd1b53cfce08135d350dc78966286a9c9e
TonyEmily Hammond <TonyEmily Hammond>
2016-10-26 21:43:40 -0500

    COD-DNW: template functionality over pixel type (put functionality into predefined function DoIt)

--- Slicer-MyExtension/Multi-LevelRegistration/Multi-LevelRegistration.cxx
+++ Slicer-MyExtension/Multi-LevelRegistration/Multi-LevelRegistration.cxx
@@ -17,83 +17,13 @@
 // thing should be in an anonymous namespace except for the module
 // entry point, e.g. main()
 //
-/*
+
 namespace
 {
 
 template <typename TPixel>
 int DoIt( int argc, char * argv[], TPixel )
 {
-  PARSE_ARGS;
-
-  return EXIT_SUCCESS;
-}
-
-} // end of anonymous namespace
-*/
-
-int main( int argc, char * argv[] )
-{
-  /*PARSE_ARGS;
-
-  itk::ImageIOBase::IOPixelType     pixelType;
-  itk::ImageIOBase::IOComponentType componentType;
-
-  try
-    {
-    itk::GetImageType(inputVolume, pixelType, componentType);
-
-    // This filter handles all types on input, but only produces
-    // signed types
-    switch( componentType )
-      {
-      case itk::ImageIOBase::UCHAR:
-        return DoIt( argc, argv, static_cast<unsigned char>(0) );
-        break;
-      case itk::ImageIOBase::CHAR:
-        return DoIt( argc, argv, static_cast<signed char>(0) );
-        break;
-      case itk::ImageIOBase::USHORT:
-        return DoIt( argc, argv, static_cast<unsigned short>(0) );
-        break;
-      case itk::ImageIOBase::SHORT:
-        return DoIt( argc, argv, static_cast<short>(0) );
-        break;
-      case itk::ImageIOBase::UINT:
-        return DoIt( argc, argv, static_cast<unsigned int>(0) );
-        break;
-      case itk::ImageIOBase::INT:
-        return DoIt( argc, argv, static_cast<int>(0) );
-        break;
-      case itk::ImageIOBase::ULONG:
-        return DoIt( argc, argv, static_cast<unsigned long>(0) );
-        break;
-      case itk::ImageIOBase::LONG:
-        return DoIt( argc, argv, static_cast<long>(0) );
-        break;
-      case itk::ImageIOBase::FLOAT:
-        return DoIt( argc, argv, static_cast<float>(0) );
-        break;
-      case itk::ImageIOBase::DOUBLE:
-        return DoIt( argc, argv, static_cast<double>(0) );
-        break;
-      case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
-      default:
-        std::cerr << "Unknown input image pixel component type: ";
-        std::cerr << itk::ImageIOBase::GetComponentTypeAsString( componentType );
-        std::cerr << std::endl;
-        return EXIT_FAILURE;
-        break;
-      }
-    }
-
-  catch( itk::ExceptionObject & excep )
-    {
-    std::cerr << argv[0] << ": exception caught !" << std::endl;
-    std::cerr << excep << std::endl;
-    return EXIT_FAILURE;
-    }*/
-
 	// parse through inputs 
 	PARSE_ARGS;
 
@@ -104,7 +34,7 @@ int main( int argc, char * argv[] )
 	std::cout << std::endl;
 
 	// instantiate image and transform types
-	typedef short					PixelType;
+	typedef TPixel					PixelType;
 	typedef itk::Image< PixelType, 3 >	ImageType;
 	typedef itk::Image< unsigned char, 3 >	MaskImageType;
 	typedef itk::ScaleVersor3DTransform< double >	TransformType;
@@ -496,6 +426,75 @@ int main( int argc, char * argv[] )
 			}
 		}
 	}
+
+  return EXIT_SUCCESS;
+}
+
+} // end of anonymous namespace
+
+int main( int argc, char * argv[] )
+{
+  PARSE_ARGS;
+
+  itk::ImageIOBase::IOPixelType     pixelType;
+  itk::ImageIOBase::IOComponentType componentType;
+
+  try
+    {
+    itk::GetImageType(fixedImageFilename, pixelType, componentType);
+
+    // This filter handles all types on input, but only produces
+    // signed types
+    switch( componentType )
+      {
+      case itk::ImageIOBase::UCHAR:
+        return DoIt( argc, argv, static_cast<unsigned char>(0) );
+        break;
+      case itk::ImageIOBase::CHAR:
+        return DoIt( argc, argv, static_cast<signed char>(0) );
+        break;
+      case itk::ImageIOBase::USHORT:
+        return DoIt( argc, argv, static_cast<unsigned short>(0) );
+        break;
+      case itk::ImageIOBase::SHORT:
+        return DoIt( argc, argv, static_cast<short>(0) );
+        break;
+      case itk::ImageIOBase::UINT:
+        return DoIt( argc, argv, static_cast<unsigned int>(0) );
+        break;
+      case itk::ImageIOBase::INT:
+        return DoIt( argc, argv, static_cast<int>(0) );
+        break;
+      case itk::ImageIOBase::ULONG:
+        return DoIt( argc, argv, static_cast<unsigned long>(0) );
+        break;
+      case itk::ImageIOBase::LONG:
+        return DoIt( argc, argv, static_cast<long>(0) );
+        break;
+      case itk::ImageIOBase::FLOAT:
+        return DoIt( argc, argv, static_cast<float>(0) );
+        break;
+      case itk::ImageIOBase::DOUBLE:
+        return DoIt( argc, argv, static_cast<double>(0) );
+        break;
+      case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
+      default:
+        std::cerr << "Unknown input image pixel component type: ";
+        std::cerr << itk::ImageIOBase::GetComponentTypeAsString( componentType );
+        std::cerr << std::endl;
+        return EXIT_FAILURE;
+        break;
+      }
+    }
+
+  catch( itk::ExceptionObject & excep )
+    {
+    std::cerr << argv[0] << ": exception caught !" << std::endl;
+    std::cerr << excep << std::endl;
+    return EXIT_FAILURE;
+    }
+
+	
 
   return EXIT_SUCCESS;
 }
